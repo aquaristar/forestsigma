5a68e5491b1dde31a638a98de95e2dd082ad5235
Mike Zeen <aquaristar@gmail.com>
2020-12-07 07:41:32 +0800

    test room page

--- testroom/views.py
+++ testroom/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	if request.method == 'POST':
+		print(request.POST.dict())
 	return render(request, 'testroom.html')
